56c4adfd326a37f7dee874bad6a2680172913282
Valerii Alexandrovich <beloenko23@gmail.com>
2024-01-19 01:30:58 +0200

    Added web

---
+++ web-frontend/postcss.config.js
@@ -0,0 +1,13 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+// postcss.config.js
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
